44064a5fc1ebb21ec6ed969267268e6215fbd89d
josephx86 <simplex86@gmail.com>
2020-05-27 17:45:43 -0500

    Initial Commit

---
+++ Text Story/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Text Story
+//
+//  Created by Joseph on 5/27/20.
+//  Copyright © 2020 Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
